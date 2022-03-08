56b7e4b8a5f69f9fb7a1ffc0864a7da0e52cb3d2
Chidi Eze <49063484+Kizmelvin@users.noreply.github.com>
2022-03-08 11:10:13 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { useEffect } from "react";
+import "cloudinary-video-player/dist/cld-video-player.min.css";
+import MyPlayer from "./Components/VideoPlayer";
+
+export default function App() {
+  useEffect(() => {
+    MyPlayer();
+  }, []);
+  return (
+    <div className="App">
+      <h2>Tracking user activities on a cloudinary video</h2>
+      <div className="vid-container">
+        <video
+          controls
+          muted
+          className="cld-video-player cld-fluid"
+          id="player"
+        />
+      </div>
+    </div>
+  );
+}
